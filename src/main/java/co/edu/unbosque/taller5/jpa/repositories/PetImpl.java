3d1098f8f10727af43f7eccc5dbf0619843f877f
Juan Sebastian Vargas Ospina <vargasospinajuan@gmail.com>
2021-11-17 21:33:00 -0500

    Entitites - Repositories

---
+++ src/main/java/co/edu/unbosque/taller5/jpa/repositories/PetImpl.java
@@ -0,0 +1,147 @@
+package co.edu.unbosque.taller5.jpa.repositories;
+
+import co.edu.unbosque.taller5.jpa.entities.Owner;
+import co.edu.unbosque.taller5.jpa.entities.Pet;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class PetImpl implements PetRepository {
+
+    private EntityManager entityManager;
+
+    public PetImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public Optional<Pet> save(Pet pet) {
+        try{
+            entityManager.getTransaction().begin();
+            entityManager.persist(pet);
+            entityManager.getTransaction().commit();
+            return Optional.of(pet);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Pet> findAll() {
+        return null;
+    }
+
+    @Override
+    public Optional<Pet> findId(Integer id) {
+        Pet pet = entityManager.createQuery("SELECT o FROM Pet o WHERE o.petId = :id", Pet.class)
+                .setParameter("id", id).getSingleResult();
+        return pet != null ? Optional.of(pet) : Optional.empty();
+    }
+
+    @Override
+    public Optional<Pet> updateName(String name, Integer petId) {
+        try{
+            entityManager.getTransaction().begin();
+            Pet pet = entityManager.find(Pet.class, petId);
+            pet.setName(name);
+            entityManager.getTransaction().commit();
+
+            return Optional.of(pet);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Pet> updateSpecies(String species, Integer petId) {
+        try{
+            entityManager.getTransaction().begin();
+            Pet pet = entityManager.find(Pet.class, petId);
+            pet.setSpecies(species);
+            entityManager.getTransaction().commit();
+
+            return Optional.of(pet);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Pet> updateRace(String race, Integer petId) {
+        try{
+            entityManager.getTransaction().begin();
+            Pet pet = entityManager.find(Pet.class, petId);
+            pet.setRace(race);
+            entityManager.getTransaction().commit();
+
+            return Optional.of(pet);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Pet> updateSize(String size, Integer petId) {
+        try{
+            entityManager.getTransaction().begin();
+            Pet pet = entityManager.find(Pet.class, petId);
+            pet.setSize(size);
+            entityManager.getTransaction().commit();
+
+            return Optional.of(pet);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Pet> updateSex(String sex, Integer petId) {
+        try{
+            entityManager.getTransaction().begin();
+            Pet pet = entityManager.find(Pet.class, petId);
+            pet.setSex(sex);
+            entityManager.getTransaction().commit();
+
+            return Optional.of(pet);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Pet> updatePicture(String picture, Integer petId) {
+        try{
+            entityManager.getTransaction().begin();
+            Pet pet = entityManager.find(Pet.class, petId);
+            pet.setPicture(picture);
+            entityManager.getTransaction().commit();
+
+            return Optional.of(pet);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Pet> updatePetMicrochip(String microchip, Integer petId) {
+        try{
+            entityManager.getTransaction().begin();
+            Pet pet = entityManager.find(Pet.class, petId);
+            pet.setMicroChip(microchip);
+            entityManager.getTransaction().commit();
+
+            return Optional.of(pet);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+}
